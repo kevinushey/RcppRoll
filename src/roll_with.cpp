777cb2e6df2e19e33628bcc4fcc287a319fc81d6
kevinushey <kevinushey@gmail.com>
2014-07-11 17:04:42 -0700

    Development version

--- src/roll_with.cpp
+++ src/roll_with.cpp
@@ -1,18 +1,16 @@
-// [[Rcpp::depends(RcppArmadillo)]]
-
-#include <RcppArmadillo.h>
+#include <Rcpp.h>
 using namespace Rcpp;
 
 namespace RcppRoll {
-  
+
 template <typename T, typename U = decltype(T()[0])>
 class VectorView {
-  
+
 public:
 
   VectorView(T const& data, int offset):
     data_(data), offset_(offset) {};
-    
+
   inline const U operator[](int idx) const {
     return const_cast<const U>(data_[offset_ + idx]);
   }
@@ -21,7 +19,7 @@ private:
 
   T const& data_;
   int offset_;
-  
+
 };
 
 template <typename T>
@@ -48,7 +46,7 @@ double prod(T const& x) {
 
 template <typename Callable, typename T>
 T roll_vector_with(Callable f, T const& x, NumericVector weights, SEXP pad) {
-  
+
   if (Rf_isNull(pad)) {
     return roll_vector_with_nopad(f, x, weights);
   } else if (strcmp(CHAR(STRING_ELT(pad, 0)), "left") == 0) {
@@ -58,32 +56,32 @@ T roll_vector_with(Callable f, T const& x, NumericVector weights, SEXP pad) {
   } else {
     return roll_vector_with_nopad(f, x, weights);
   }
-  
+
 }
 
 template <typename Callable, typename T>
 T roll_vector_with_nopad(Callable f, T const& x, NumericVector weights) {
-  
+
   int x_n = x.size();
   int weights_n = weights.size();
   int n_ops = x_n - weights_n + 1;
-  
+
   T result = no_init(n_ops);
   for (int i = 0; i < n_ops; ++i) {
     result[i] = f(RcppRoll::VectorView<T>(x, i) * weights);
   }
-  
+
   return result;
-  
+
 }
 
 template <typename Callable, typename T>
 T roll_vector_with_pad_left(Callable f, T const& x, NumericVector weights) {
-  
+
   int x_n = x.size();
   int weights_n = weights.size();
   int n_ops = x_n - weights_n + 1;
-  
+
   T result = no_init(x_n);
   for (int i = 0; i < weights_n - 1; ++i) {
     result[i] = T::get_na();
@@ -91,17 +89,17 @@ T roll_vector_with_pad_left(Callable f, T const& x, NumericVector weights) {
   for (int i = 0; i < n_ops; ++i) {
     result[i + weights_n - 1] = f(VectorView<T>(x, i) * weights);
   }
-  
+
   return result;
-  
+
 }
-  
+
 template <typename Callable, typename T>
 T roll_vector_with_pad_right(Callable f, T const& x, NumericVector weights) {
-  
+
   int x_n = x.size();
   int weights_n = weights.size();
-  
+
   T result = no_init(x_n);
   for (int i = 0; i < x_n - weights_n + 1; ++i) {
     result[i] = f(VectorView<T>(x, i) * weights);
@@ -109,24 +107,24 @@ T roll_vector_with_pad_right(Callable f, T const& x, NumericVector weights) {
   for (int i = x_n - weights_n + 1; i < x_n; ++i) {
     result[i] = T::get_na();
   }
-  
+
   return result;
-  
+
 }
 
 template <typename Callable, typename T>
 T roll_matrix_with(Callable f, T const& x, NumericVector weights, SEXP pad) {
-  
+
   int nrow = x.nrow();
   int ncol = x.ncol();
-  
+
   T output(nrow - weights.size() + 1, ncol);
   for (int i = 0; i < ncol; ++i) {
     output(_, i) = roll_vector_with(f, static_cast<NumericVector>(x(_, i)), weights, pad);
   }
-  
+
   return output;
-  
+
 }
 
 struct mean_f {
@@ -184,7 +182,12 @@ struct var_f {
     }
     return sum_f()(normalized) / (n - 1);
   }
-  
+};
+
+struct sd_f {
+  inline double operator()(std::vector<double> const& x) {
+    return sqrt(var_f()(x));
+  }
 };
 
 std::vector<double> square(std::vector<double> const& x, int exponent) {
@@ -196,36 +199,68 @@ std::vector<double> square(std::vector<double> const& x, int exponent) {
   return result;
 }
 
-inline double var(std::vector<double> const& x) {
-  return sum_f()(mean_normalize(x)) / (x.size() - 1);
-}
+struct median_f {
+  inline double operator()(std::vector<double> x) {
+
+    double median;
+
+    size_t size = x.size();
+    sort(x.begin(), x.end());
+
+    if (size % 2 == 0) {
+      median = (x[size / 2 - 1] + x[size / 2]) / 2;
+    } else {
+      median = x[size / 2];
+    }
+
+    return median;
+  }
+};
 
 } // end namespace RcppRoll
 
 using namespace RcppRoll;
 
+#define DISPATCH(__FUNCTION__) \
+  if (Rf_isMatrix(x)) { \
+    return roll_matrix_with(__FUNCTION__##_f(), NumericMatrix(x), weights, pad); \
+  } else { \
+    return roll_vector_with(__FUNCTION__##_f(), NumericVector(x), weights, pad); \
+  }
+
 // [[Rcpp::export]]
 SEXP roll_mean(SEXP x, SEXP weights, SEXP pad) {
-  if (Rf_isMatrix(x)) {
-    return roll_matrix_with(mean_f(), NumericMatrix(x), weights, pad);
-  } else {
-    return roll_vector_with(mean_f(), NumericVector(x), weights, pad);
-  }
+  DISPATCH(mean);
+}
+
+// [[Rcpp::export]]
+SEXP roll_median(SEXP x, SEXP weights, SEXP pad) {
+  DISPATCH(median);
 }
 
 // [[Rcpp::export]]
 SEXP roll_max(NumericVector x, NumericVector weights, SEXP pad) {
-  return roll_vector_with(max_f(), x, weights, pad);
+  DISPATCH(max);
 }
 
 // [[Rcpp::export]]
 SEXP roll_min(NumericVector x, NumericVector weights, SEXP pad) {
-  return roll_vector_with(min_f(), x, weights, pad);
+  DISPATCH(min);
 }
 
 // [[Rcpp::export]]
 SEXP roll_prod(NumericVector x, NumericVector weights, SEXP pad) {
-  return roll_vector_with(prod_f(), x, weights, pad);
+  DISPATCH(prod);
+}
+
+// [[Rcpp::export]]
+SEXP roll_sd(NumericVector x, NumericVector weights, SEXP pad) {
+  DISPATCH(sd);
+}
+
+// [[Rcpp::export]]
+SEXP roll_var(NumericVector x, NumericVector weights, SEXP pad) {
+  DISPATCH(var);
 }
 
 /*** R
@@ -239,6 +274,7 @@ pad <- "right"
 roll_mean(x, weights, pad)
 x <- rnorm(1E4)
 library(microbenchmark)
+library(zoo)
 microbenchmark(
   roll_mean(x, weights, NULL),
   rollapply(x, weights, mean),
