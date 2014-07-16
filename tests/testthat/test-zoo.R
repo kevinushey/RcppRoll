9c4a3f0204fd5bdc4ebfe4c78ecf3a71b3a16f74
Kevin Ushey <kevinushey@gmail.com>
2014-07-15 23:09:44 -0700

    Test over more arguments

--- tests/testthat/test-zoo.R
+++ tests/testthat/test-zoo.R
@@ -29,4 +29,16 @@ test_that("we behave similarly to zoo::rollapply", {
   x <- rnorm(1E3, mean = 1E200, sd = 1E201)
   run_tests(x, 5L)
 
+  ## now let's really stress it...
+  args <- expand.grid(
+    width = list(3L, 10L, 100L),
+    fill = list(NA, c(-1, 0, 1)),
+    align = list("left", "center", "right")
+  )
+
+  data <- rnorm(1E2, 100, 50)
+  for (i in 1:nrow(args)) {
+    run_tests(data, args$width[[i]], fill = args$fill[[i]], align = args$align[[i]])
+  }
+
 })
